613445a1a4ea941cf0535a70afc67685eaecda43
Nikithareddy-2027 <nikitha0520@gmail.com>
2022-02-07 18:21:05 +0530

    Add project files.

---
+++ HMS.DAL/Migrations/20220207113422_jh.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HMS.DAL.Migrations
+{
+    public partial class jh : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_pharmacy_doctor_DoctorId",
+                table: "pharmacy");
+
+            migrationBuilder.DropIndex(
+                name: "IX_pharmacy_DoctorId",
+                table: "pharmacy");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PatientId",
+                table: "pharmacy",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DoctorId",
+                table: "pharmacy",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DoctorsDoctorId",
+                table: "pharmacy",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_pharmacy_DoctorsDoctorId",
+                table: "pharmacy",
+                column: "DoctorsDoctorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_pharmacy_doctor_DoctorsDoctorId",
+                table: "pharmacy",
+                column: "DoctorsDoctorId",
+                principalTable: "doctor",
+                principalColumn: "DoctorId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_pharmacy_doctor_DoctorsDoctorId",
+                table: "pharmacy");
+
+            migrationBuilder.DropIndex(
+                name: "IX_pharmacy_DoctorsDoctorId",
+                table: "pharmacy");
+
+            migrationBuilder.DropColumn(
+                name: "DoctorsDoctorId",
+                table: "pharmacy");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PatientId",
+                table: "pharmacy",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DoctorId",
+                table: "pharmacy",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_pharmacy_DoctorId",
+                table: "pharmacy",
+                column: "DoctorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_pharmacy_doctor_DoctorId",
+                table: "pharmacy",
+                column: "DoctorId",
+                principalTable: "doctor",
+                principalColumn: "DoctorId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ HMS.Entity/Models/Pharmacy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace HMS.Entity.Models
+{
+    public class Pharmacy
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MedicineId { get; set; }
+
+        [ForeignKey("PatientReg")]
+        public string PatientId { get; set; }
+        public virtual PatientReg PatientRegs { get; set; }
+
+        [ForeignKey("Doctor")]
+        public string DoctorId { get; set; }
+        public virtual Doctor Doctors { get; set; }
+        
+        public string MedicineName { get; set; }
+        public string MedicineIssue { get; set; }
+        public string MedicineTime { get; set; }
+        public bool pharmacyStock { get; set; }
+    }
+}
